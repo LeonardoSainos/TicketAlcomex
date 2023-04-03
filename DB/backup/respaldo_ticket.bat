69563dd45464b463ea0a35783c25cb2a0e82af7d
Leonardo Sainos <leonardosainos@gmail.com>
2023-04-03 12:22:37 -0600

    Copias de seguridad diarias

---
+++ DB/backup/respaldo_ticket.bat
@@ -0,0 +1,18 @@
+echo off
+set fecha1=%date%
+set fecha2=%date:/=_%
+set fecha3=%date:/=%
+set hora1=%time%
+set hora2=%time::=.%
+set hora3=%time::=%
+set hora4=%hora3:~0,6%
+ 
+set nombre=%1_%fecha3%_%hora4%.sql
+echo %nombre%;
+
+cd C:\xampp\mysql\bin
+
+mysqldump -u root %1 > C:\xampp\htdocs\TL11EST.github.io\DB\backup\ticketdb\%nombre%
+
+pause > nul
+
