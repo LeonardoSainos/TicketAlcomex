b3fefbaacc6b3ce76fed8eef5a4415142fccbd07
Leonardo Sainos <leonardosainos@gmail.com>
2023-04-14 17:01:39 -0600

    Nuevo

--- TICKET/inc/timezone.php
+++ TICKET/inc/timezone.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_set('America/Mazatlan');
+date_default_timezone_set('America/Mexico_City');
 setlocale(LC_TIME, 'spanish');
 echo utf8_encode(strftime("%A %#d de %B del %Y"));
 ?>
